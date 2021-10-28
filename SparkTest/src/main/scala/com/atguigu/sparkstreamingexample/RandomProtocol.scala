6b908a31be845b68859b7ed836b26ca2a5f1f93f
lucienOz <brewstername@live.com>
2021-10-28 19:15:35 +0800

    second

---
+++ SparkTest/src/main/scala/com/atguigu/sparkstreamingexample/RandomProtocol.scala
@@ -0,0 +1,16 @@
+package com.atguigu.sparkstreamingexample
+
+/**
+ *
+ * Description:
+ *
+ * Create by lucienoz on 2021/10/26.
+ * Copyright © 2021 lucienoz. All rights reserved.
+ */
+trait RandomProtocol {
+
+	//拿到对应的随机数
+	def getRandomNum(scope:Int):Int
+
+
+}
